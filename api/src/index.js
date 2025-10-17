c75a2b381e811753b440d1db4be1d1a0291811e3
Vikicha089 <vbanchevf@gmail.com>
2025-10-17 23:41:24 +0300

    Install and setup express

--- api/src/index.js
+++ api/src/index.js
@@ -1,1 +1,11 @@
-console.log("Works");
+import express from 'express';
+
+const app = express()
+
+app.get("/", (req, res) => {
+    console.log("Works");
+    res.send("It works")
+});
+
+app.listen(3030, () => console.log("Server is listening on http://localhost:3030..."))
+
