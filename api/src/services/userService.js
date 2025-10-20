f6078a5d1e2cd9b193297b5126723490d3131bd0
Vikicha089 <vbanchevf@gmail.com>
2025-10-20 21:23:11 +0300

    Add login on register

--- api/src/services/userService.js
+++ api/src/services/userService.js
@@ -1,10 +1,18 @@
 import User from "../models/User.js"
 import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import { generateAuthToken } from "../utils/tokenUtils.js";
 
 export default {
-    register({ email, password }) {
-        return User.create({ email, password })
+    async register({ email, password }) {
+        const user = await User.create({ email, password });
+
+        const token = generateAuthToken(user);
+        return {
+            _id: user.id,
+            email: user.email,
+            accessToken: token,
+        };
+
     },
     async login({ email, password }) {
         const user = await User.findOne({ email });
@@ -20,14 +28,9 @@ export default {
         }
 
         //Generate token
-        const payload = {
-            id: user.id,
-            email: user.email,
-        }
-
-        const token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: '2h'})
+        const token = generateAuthToken(user);
         return {
-            _id: user.id, 
+            _id: user.id,
             email: user.email,
             accessToken: token,
         };
